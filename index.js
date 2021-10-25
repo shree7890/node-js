b0709f2bf06f88f0828a981514fe35d5d62844a8
shree7890 <shreekantadash831@gmail.com>
2021-10-26 01:14:10 +0600

    my-first-commit

---
+++ index.js
@@ -0,0 +1,170 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const port = process.env.PORT || 5000
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('Hello world my learning system')
+})
+app.use(cors())
+const users = [
+    {
+        "id": 0,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 1,
+        "name": "shreekanta",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "kantashree",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 3,
+        "name": "sudhin",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 4,
+        "name": "tonoy",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+
+]
+
+app.get('/fruits', (req, res) => {
+    const fruits = ["mango", "banana", "lichi", "pineapple", "apple"]
+    res.send(fruits)
+})
+
+app.get('/fruits/mangoes/fazli', (req, res) => {
+    const fazli = "amara fazli am vala pai re bondu"
+    res.send(fazli)
+})
+//app method
+app.post('/users', (req, res) => {
+    const newUser = req.body
+    newUser.id = users.length
+    users.push(newUser)
+    console.log("usr hitted by post", req.body)
+    res.json(newUser)
+})
+
+// query akare dekanu hoice data
+
+app.get('/users', (req, res) => {
+    const search = req.query.search;
+    if (search) {
+        const searchResult = users.filter(user => user.name.toLowerCase().includes(search));
+        res.send(searchResult)
+    }
+    else {
+        res.send(users)
+    }
+})
+
+
+// dynamic vabe dekanu url
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    const user = users[id]
+    res.send(user)
+})
+app.listen(port, () => {
+    console.log('listening to port', port)
+})
